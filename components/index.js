02217be04ad8db6972b7354c5a1641401da3087b
Indra Pranata <indrapranata120@gmail.com>
2022-08-29 15:07:29 +0800

    Finishing PostCard Component

--- components/index.js
+++ components/index.js
@@ -1,4 +1,5 @@
 export { default as Categories } from './Categories'
+export { default as DateIcon } from './DateIcon'
 export { default as Header } from './Header'
 export { default as Layout } from './Layout'
 export { default as PostCard } from './PostCard'
