135b281fd42931a4d37fd13baa3557c2b9c81ba4
Indra Pranata <indrapranata120@gmail.com>
2022-08-31 11:48:31 +0800

    Create Author component

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,7 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
 
-export default function Author({author}) {
+export default function Author({ author }) {
   return (
-    <div>Author</div>
+    <div className='text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20'>
+      <div className='absolute left-0 right-2 -top-12'>
+        <Image
+          src={author.profilePicture.url}
+          alt={author.name}
+          height={80}
+          width={80}
+        />
+      </div>
+      <h3 className='text-white my-4 text-xl font-bold'>{author.name}</h3>
+      <p className='text-white text-lg'>{author.bio}</p>
+    </div>
   )
 }
